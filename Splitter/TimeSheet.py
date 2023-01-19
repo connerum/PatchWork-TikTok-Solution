d6f7d7e2035550f5502070de53a1c9c127e4272b
connerum <98507011+connerum@users.noreply.github.com>
2023-01-19 01:37:12 -0500

    Added TimeSheet.py

---
+++ Splitter/TimeSheet.py
@@ -0,0 +1,11 @@
+def main():
+    start = 0
+    end = 120
+    for i in range(360):
+        print(f"{start}-{end}")
+        start += 120
+        end += 120
+
+
+if __name__ == "__main__":
+   main()
