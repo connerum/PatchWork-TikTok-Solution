bef82b30440e7f61b8b8b2aacfaeda5472113cae
connerum <98507011+connerum@users.noreply.github.com>
2023-01-28 01:42:53 -0500

    Remodeled Splitter.py

--- Splitter/Splitter.py
+++ Splitter/Splitter.py
@@ -1,15 +1,40 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip
 
-# Replace the filename below.
-required_video_file = "UnsplitVideo/unsplit.mp4"
+def main():
+    segmentInput = int(input("Enter Segment Lengths in Seconds (Ex. 1 minute = 60): "))
+    splitVideo(segmentInput)
 
-with open("times.txt") as f:
-    times = f.readlines()
+def splitVideo(segment):
+    timeSheetList = getTimeSheet(segment)
 
-times = [x.strip() for x in times]
+    i = 0
+    for time in timeSheetList:
+        starttime = int(time.split("-")[0])
+        endtime = int(time.split("-")[1])
+        ffmpeg_extract_subclip(getVideoPath(), starttime, endtime,
+                               targetname="SplitVideos/" + (str((i + 1)) + ".mp4"))
+        i += 1
 
-for time in times:
-    starttime = int(time.split("-")[0])
-    endtime = int(time.split("-")[1])
-    ffmpeg_extract_subclip(required_video_file, starttime, endtime,
-                           targetname="SplitVideos/" + (str(times.index(time) + 1) + ".mp4"))
+def getVideoPath():
+    return "UnsplitVideo/unsplit.mp4"
+
+def getVideoCount(segment):
+    vidDuration = VideoFileClip(getVideoPath()).duration
+    vidCal = (int(vidDuration) / segment)
+    return vidCal
+
+def getTimeSheet(segment):
+    newTimeSheet = []
+    start = 0
+    end = segment
+    for i in range(int(getVideoCount(segment))):
+        newTimeSheet.append(f"{start}-{end}")
+        start += segment
+        end += segment
+    return newTimeSheet
+
+
+if __name__ == "__main__":
+    main()
+
